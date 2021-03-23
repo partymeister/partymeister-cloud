1b2953183832e00c7fb288e819e2a7eb05024381
Reza Esmaili <github@dfox.info>
2021-03-23 17:22:04 +0100

    Add logo

--- config/motor-backend-project.php
+++ config/motor-backend-project.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'name'        => 'PartyMeister Cloud Service',
-    'name_part_1' => 'Partymeister',
-    'name_part_2' => 'Cloud Service',
-    'name_short'  => 'PMC',
-    'copyright'   => 'Reza Esmaili',
-    'logo-large'  => 'images/pm-logo-large.png',
-    'logo-small'  => 'images/pm-logo-small.png',
-    'logo-white'  => 'images/pm-logo-small-white.png'
+    'name'               => 'PartyMeister Cloud Service',
+    'name_part_1'        => 'Partymeister',
+    'name_part_2'        => 'Cloud Service',
+    'name_short'         => 'PMC',
+    'copyright'          => 'Reza Esmaili',
+    'logo-large'         => 'images/pm-logo-large.png',
+    'logo-small'         => 'images/pm-logo-small.png',
+    'logo-white'         => 'images/pm-logo-small-white.png',
+    'logo-white-cropped' => 'images/pm-logo-small-white-cropped.png',
 ];
