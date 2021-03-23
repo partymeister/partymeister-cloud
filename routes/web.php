5ae2361b780b32cc3ff2169e43f6dcd98a508fec
Reza Esmaili <github@dfox.info>
2021-03-23 17:08:50 +0100

    Add permission middleware

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,36 @@
 Route::group([
     'as'         => 'backend.',
     'prefix'     => 'backend',
-    'namespace'  => 'App\Backend',
+    'namespace'  => 'Backend',
     'middleware' => [
         'web',
         'web_auth',
         'navigation'
     ]
-], function () {
-    Route::resource('projects', 'ProjectsController');
-    Route::resource('websites', 'WebsitesController');
-    Route::resource('apps', 'AppsController');
+], static function () {
+    Route::group([ 'middleware' => [ 'permission' ] ], static function () {
+        Route::resource('projects', 'ProjectsController');
+        Route::resource('websites', 'WebsitesController');
+        Route::resource('apps', 'AppsController');
 
-    Route::resource('project_navigations', 'ProjectNavigationsController', [
-        'except' => [
-            'index',
-            'create'
-        ]
-    ]);
+        Route::resource('project_navigations', 'ProjectNavigationsController', [
+            'except' => [
+                'index',
+                'create'
+            ]
+        ]);
 
-    Route::get('project_navigations/{project_navigation}', 'ProjectNavigationsController@index')
-         ->name('project_navigations.index');
-    Route::get('project_navigations/{project_navigation}/create', 'ProjectNavigationsController@create')
-         ->name('project_navigations.create');
+        Route::get('project_navigations/{project_navigation}', 'ProjectNavigationsController@index')
+             ->name('project_navigations.index');
+        Route::get('project_navigations/{project_navigation}/create', 'ProjectNavigationsController@create')
+             ->name('project_navigations.create');
 
-    Route::resource('project_navigation_trees', 'ProjectNavigationTreesController', [
-        'parameters' => [
-            'project_navigation_trees' => 'project_navigation'
-        ]
-    ]);
-    Route::get('project_navigation_trees/{project_navigation}/duplicate', 'ProjectNavigationTreesController@duplicate')
-         ->name('project_navigation_trees.duplicate');
+        Route::resource('project_navigation_trees', 'ProjectNavigationTreesController', [
+            'parameters' => [
+                'project_navigation_trees' => 'project_navigation'
+            ]
+        ]);
+        Route::get('project_navigation_trees/{project_navigation}/duplicate', 'ProjectNavigationTreesController@duplicate')
+             ->name('project_navigation_trees.duplicate');
+    });
 });
